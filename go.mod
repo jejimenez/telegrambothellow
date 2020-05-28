afcd3e9fe48f0a6487dc6db4e9c6c245f7cfd2c1
Jaime Jiménez <jimenez.ing.sis@gmail.com>
2020-05-27 21:58:04 -0500

    heroku

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/jejimenez/telegrambothellow
 
 go 1.13
+
+require gopkg.in/tucnak/telebot.v2 v2.0.0-20200522115256-76430783a7a2 // indirect
--- main.go
+++ main.go
@@ -1,19 +1,44 @@
 package main
 
 import (
-    "os"
     tb "gopkg.in/tucnak/telebot.v2"
+    "log"
+    "os"
 )
 
 func main() {
+    f, err := os.OpenFile("testlogfile", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+    if err != nil {
+        log.Fatalf("error opening file: %v", err)
+    }
+    defer f.Close()
+
+    log.SetOutput(f)
+    log.Println("This is a test log entry")
     var (
-        port      = os.Getenv("PORT")       // sets automatically
-        publicURL = os.Getenv("PUBLIC_URL") // you must add it to your config vars
-        token     = os.Getenv("TOKEN")      // you must add it to your config vars
+        port      = os.Getenv("PORT")
+        publicURL = os.Getenv("PUBLIC_URL")
+        token     = os.Getenv("TOKEN")
     )
 
     webhook := &tb.Webhook{
         Listen:   ":" + port,
         Endpoint: &tb.WebhookEndpoint{PublicURL: publicURL},
     }
+
+    pref := tb.Settings{
+        Token:  token,
+        Poller: webhook,
+    }
+
+    b, err := tb.NewBot(pref)
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    b.Handle("/hello", func(m *tb.Message) {
+        b.Send(m.Sender, "Hi!")
+    })
+
+    b.Start()
 }
